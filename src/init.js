e280d6c805c0d479d996d4b06641ab85831ad145
lelderbe <fvdc@ya.ru>
2022-06-08 21:54:08 +0300

    feat: add arguments validation

--- src/init.js
+++ src/init.js
@@ -5,10 +5,16 @@ import { chdir } from 'process';
 export const state = {};
 
 console.log('init');
-state.args = parseArgs();
+
+state.username = parseArgs().username;
+
+if (!state.username) {
+	console.log('Error: wrong arguments. Use: --username=your_name');
+	process.exit(0);
+}
+
 state.home = os.homedir();
 state.pwd = os.homedir();
-state.username = state.args?.username;
 
 try {
 	chdir(state.pwd);
