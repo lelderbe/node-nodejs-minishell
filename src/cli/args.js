b55faffd4b758eb26cd328b8752ccae9bc710fe8
lelderbe <fvdc@ya.ru>
2022-06-12 11:26:36 +0300

    fix: check prefix '--' for given arguments

--- src/cli/args.js
+++ src/cli/args.js
@@ -4,8 +4,10 @@ export const parseArgs = () => {
 	const dict = {};
 
 	for (const arg of args) {
-		const [key, value] = arg.split('=');
-		dict[key.slice(2)] = value;
+		if (arg.startsWith('--')) {
+			const [key, value] = arg.split('=');
+			dict[key.slice(2)] = value;
+		}
 	}
 
 	return dict;
